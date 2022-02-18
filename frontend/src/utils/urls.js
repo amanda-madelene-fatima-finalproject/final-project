d4c2f4d706afe0348654c0f65a251040f2a2b28e
MT-dotse <madelene.trang333@gmail.com>
2022-02-18 18:41:04 +0200

    removed slash from utils

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://debug-happy-habiting.herokuapp.com/';
+const BASE_URL = 'http://https://debug-happy-habiting.herokuapp.com';
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
