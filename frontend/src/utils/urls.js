f9c7b616c9b7e2e347979b90b8008a0fd8227b48
MT-dotse <madelene.trang333@gmail.com>
2022-02-18 18:47:16 +0200

    resolving heroku link

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'http://debug-happy-habiting.herokuapp.com';
+const BASE_URL = 'https://debug-happy-habiting.herokuapp.com';
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
